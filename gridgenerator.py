f4c70851471edc67442359fe8460ead753cc8add
Mohammad Yasir <h_md.yasir@outlook.in>
2022-12-26 11:33:55 +0530

    Adapted for import use-case

--- gridgenerator.py
+++ gridgenerator.py
@@ -11,23 +11,32 @@ def print2DMatrix(matrix):
 
 
 # Generate a random grid with a given occupation probability. Note that the probability effects kick in best when size is very large.
-def oneDimensionRandom(occupationProbability, size):
+def oneDimensionRandom():
     # A random number being smaller than occupation probability is considered to be the criterion for the site to be occupied.
+    
+    occupationProbability = float(input("\nEnter the occupation probability desired... "))
+    size = int(input("Enter the length of the matrix... "))
     grid = [uniform(0, 1) <= occupationProbability for i in range(size)]
     return grid
 
 
 # This time, let the user enter a one dimensional lattice.
-def oneDimensionInput():
-    raw = list(map(int, str(input("Enter the grid information in terms of 0 and 1, separated by comma (,)... ")).split(",")))
+def oneDimensionInput(tab = False):
+    message ="{0}Enter the grid information in terms of 0 and 1, separated by comma (,)... ".format("\t" if tab else "")
+    raw = list(map(int, str(input(message)).split(",")))
     return raw
 
 
 # Same as oneDimensionRandom(), but in two dimensions
 # The parameter size is a tuple,
-def twoDimensionRandom(occupationProbability, size):
+def twoDimensionRandom():
+
+    occupationProbability = float(input("\nEnter the occupation probability desired... "))
+    size = list(map(int, str(input("Enter the number of rows and columns, separated by comma only... ")).split(",")))
+
     rows = size[0]
     columns = size[1]
+
     grid = [[0 for i in range(rows)] for j in range(columns)]
     for i in range(rows):
         for j in range(columns):
@@ -43,7 +52,7 @@ def twoDimensionInput():
     grid = [[]]*rows
     # We can simply reuse the oneDimensionInput() function for each row of the lattice.
     for i in range(rows):
-        print("Row {0} input:".format(i))
-        grid[i] = oneDimensionInput()
+        print("Row number {0}:".format(i))
+        grid[i] = oneDimensionInput(True)
     
     return grid
